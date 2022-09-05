c85252fb7daa57da0290e83bb8817d418e80c942
leonardo silva pinheiro <leonardo.thsp@gmail.com>
2022-09-05 14:40:22 -0300

    removido contato e sobre

--- recipes/views.py
+++ recipes/views.py
@@ -6,11 +6,3 @@ def home(request):
     return render(request, 'recipes/home.html', context={
         'name': 'leonardo',
     })
-
-
-def contato(request):
-    return render(request, 'tmp.html')
-
-
-def sobre(request):
-    return HttpResponse('Sobre')
