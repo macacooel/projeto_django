59ea1e16a66e08bc23cf7445d050a7f1e98696ce
leonardo silva pinheiro <leonardo.thsp@gmail.com>
2022-10-10 16:27:08 -0300

    Models

--- recipes/views.py
+++ recipes/views.py
@@ -1,14 +1,16 @@
 
 from django.shortcuts import render
+from ultis.recipes.factory import make_recipe
 
 
 def home(request):
     return render(request, 'pages/home.html', context={
-        'name': 'leonardo',
+        'recipes': [make_recipe() for _ in range(3)],
     })
 
 
 def recipe(request, id):
     return render(request, 'pages/recipe-view.html', context={
-        'name': 'leonardo',
+        'recipe': make_recipe(),
+
     })
